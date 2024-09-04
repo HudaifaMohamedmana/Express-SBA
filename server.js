b6d73b6465cbd207ee722c6003fb6ed3d6b28139
hudaifa mohamedmana <hudaifamana23@gmail.com>
2024-09-04 18:00:02 -0400

    middleware

--- server.js
+++ server.js
@@ -3,10 +3,20 @@ const app = express();
 const posts = require('./routes/posts')
 //=================--------------------------------==========================
 app.set('view engine','ejs')
+//-----------------------------   middleware    -------------------------------------
+app.use((req, res, next) => {
+    console.log('Request received:', req.method, req.url);
+    next();
+});
+
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('bad reqest, Something went wrong!'); 
+    next();
+});
+// --------------------------------- routes ------------------------------
 
 app.use('/',posts)
-
-
 
 //==========================-------------------==============================
 
